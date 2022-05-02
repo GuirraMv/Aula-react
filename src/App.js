313ff9ca82971041c65abc16a9e8b85de130857e
MATHEUS <cg3014967@LAB117M16.IFSP>
2022-05-02 17:56:02 -0300

    Organizando estudos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import Header from "./Components/Header";
 import { Route, Routes } from "react-router-dom";
 import NotFound from './pages/NotFound';
+import APIIntegration from "./Estudo/APIIntegration";
 
 
 function App() {
   return (
     <>
       <div className="App">
+      <APIIntegration />
       <header className="Header">
           <Header/>
         </header>
-
+        
 
          {/* <Routes>
           <Route path="*" element={<NotFound/>} />
