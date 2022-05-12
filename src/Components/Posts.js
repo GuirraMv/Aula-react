4f8297cbdb48e1b3956e790274dc01074e9f906c
GuirraMv <m.guirra@aluno.ifsp.edu.br>
2022-05-12 12:31:52 -0300

    cardsbootstrap

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -2,68 +2,66 @@ import './Posts.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 const Posts = (props) => {
-     const {id, name, lastname, avatar, status} = props;    
-   return (
-       <>
-       <h1 className="Título">Posts</h1>
-    <div className="Posts">
-        <Container>
-        <Row>
-            <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
+    //  const {id, name, lastname, avatar, status} = props;
+    return(
+        <>
+        <h1 className="Título">Posts</h1>
+     
+     <div className="Posts">
+         
+         <CardGroup>
+   <Card>
+     <Card.Img variant="top" src="holder.js/100px160" />
+     <Card.Body>
+       <Card.Title>Card title</Card.Title>
+       <Card.Text>
+         This is a wider card with supporting text below as a natural lead-in to
+         additional content. This content is a little bit longer.
+       </Card.Text>
+     </Card.Body>
+     
+     
+     <Card.Footer>
+       <small className="text-muted">Last updated 3 mins ago</small>
+     </Card.Footer>
+   </Card>
+   <Card>
+     <Card.Img variant="top" src="holder.js/100px160" />
+     <Card.Body>
+       <Card.Title>Card title</Card.Title>
+       <Card.Text>
+         This card has supporting text below as a natural lead-in to additional
+         content.{' '}
+       </Card.Text>
+     </Card.Body>
+     <Card.Footer>
+       <small className="text-muted">Last updated 3 mins ago</small>
+     </Card.Footer>
+   </Card>
+   <Card>
+     
+     <Card.Img variant="top" src="holder.js/100px160" />
+     <Card.Body>
+       <Card.Title>Card title</Card.Title>
+       <Card.Text>
+         This is a wider card with supporting text below as a natural lead-in to
+         additional content. This card has even longer content than the first to
+         show that equal height action.
+       </Card.Text>
+     </Card.Body>
+     <Card.Footer>
+       <small className="text-muted">Last updated 3 mins ago</small>
+     </Card.Footer>
+   </Card>
+ </CardGroup>
 
-        <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
-
-        <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
-
-
-        <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
-
-        <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
-
-        <Col sm>
-        <div data-id={id} className="cardavatar">
-            <img src={avatar} alt={name+' '+lastname} width="76px" height="76px"/>
-            <h1>{name} {lastname}</h1>
-            <p>{status}</p>
-        </div>
-        </Col>
-        </Row>
-        </Container>
-    </div>
-    </>
-   );
- }
+ </div>
+ </>     
+    )
+}
 
 export default Posts
