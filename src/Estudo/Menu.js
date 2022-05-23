4dd74be42d05628998e4f9ce615c2a874979440b
MATHEUS <cg3014967@LAB117M09.IFSP>
2022-05-23 16:48:43 -0300

    atualizando estudos

--- src/Estudo/Menu.js
+++ src/Estudo/Menu.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import './Menu.css'
+import { useAuth } from '../providers/authProvider'
 
 const Menu = () => {
+
+  const {isLogged, userLogged, setIsLogged, setUserLogged} = useAuth();
+
+  const logout = () => {
+    setIsLogged(false)
+    setUserLogged({})
+    localStorage.removeItem('userLogged')
+  }
+
   return (
     <div className='menu'>
         Menu: <NavLink to="/">Home</NavLink> 
         <NavLink end style={{margin: '0 0 0 10px'}} to="images">Images</NavLink> 
-        <NavLink style={{margin: '0 0 0 10px'}} to="api-integration">Api Integration</NavLink> 
         <NavLink style={{margin: '0 0 0 10px'}} to="props">Props</NavLink> 
         <NavLink style={{margin: '0 0 0 10px'}} to="children">Children</NavLink> 
-        <NavLink style={{margin: '0 0 0 10px'}} to="images/icon">Icon</NavLink> 
+        <NavLink style={{margin: '0 0 0 10px'}} to="images/icon">Icon</NavLink>  
+        { isLogged 
+          ? (
+              <>
+                <span>Logado como: {userLogged.email}</span> 
+                <button onClick={logout}>Sair</button>
+              </>
+          )
+          : (<span><Link to="admin/login">Login Admin</Link></span>)
+        }
+        
     </div>
 
   )
